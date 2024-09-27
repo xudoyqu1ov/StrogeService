7b39b569c87566ffb621a5f9c8b6a6dcde1da448
Abdulaziz <khudoyqulov.04@gmail.com>
2024-09-27 09:01:38 +0300

    Initial commit

---
+++ src/main/java/uz/pdp/strogeservice/StrogeServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.strogeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrogeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrogeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/strogeservice/StrogeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.strogeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrogeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
